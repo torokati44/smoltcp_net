da4b02a6c475e52c8e3fc65167ba2a80b7854a2f
TÖRÖK Attila <torokati44@gmail.com>
2017-09-26 01:23:02 +0200

    make it work with INET 4.0

--- SmolTcpStack.cc
+++ SmolTcpStack.cc
@@ -1,9 +1,14 @@
 
 #include "SmolTcpStack.h"
 
-#include "inet/common/serializer/headerserializers/ethernet/EthernetSerializer.h"
 #include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/common/packet/chunk/BytesChunk.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/common/Units.h"
 
+#include "inet/common/serializer/EthernetCRC.h"
+#include "inet/linklayer/ethernet/EtherFrame_m.h"
+#include "inet/linklayer/ethernet/Ethernet.h"
 
 extern "C" {
     void init_smoltcp_logging();
@@ -26,6 +31,7 @@ extern "C" {
         // maps the Rust loglevel to opp loglevel
         static const LogLevel levelMapping[] = { LOGLEVEL_OFF, LOGLEVEL_ERROR,
                 LOGLEVEL_WARN, LOGLEVEL_INFO, LOGLEVEL_DEBUG, LOGLEVEL_TRACE };
+        printf("LOOOG %s\n", text);
         EV_LOG(levelMapping[level], "smoltcp_c") << text << std::endl;
     }
 
@@ -41,19 +47,30 @@ Define_Module(SmolTcpStack);
 void SmolTcpStack::sendEthernetFrame(unsigned const char *data, unsigned int size) {
     EV_TRACE << "smoltcp wants to send " << size << " bytes in module " << getId() << endl;
 
-    inet::serializer::EthernetSerializer ser;
-    inet::serializer::Context ctx;
-
     const int N = 2048;
     unsigned char bytes[N];
+
     // have to pad with zeros
-    for(int i = 0; i < N; ++i)
+    for(int i = 0; i < size; ++i)
         bytes[i] = data[i];
 
-    // because the frame can't be shorter than 64 bytes, even if the payload is less
-    inet::serializer::Buffer buf((void*)bytes, std::max(size+4, 64u));
+    for (; size < MIN_ETHERNET_FRAME_BYTES-4; ++size)
+        bytes[size] = 0;
 
-    send(ser.deserializePacket(buf, ctx), "ethOut");
+    auto chunk = std::make_shared<inet::BytesChunk>(bytes, size); // bytes, N ??
+    chunk->markImmutable();
+    auto pkt = new inet::Packet("aa", chunk);
+
+    auto ethFcs = std::make_shared<inet::EthernetFcs>();
+    ethFcs->setFcsMode(inet::FCS_COMPUTED);
+
+    auto computedFcs = inet::serializer::ethernetCRC(bytes, size);
+    ethFcs->setFcs(computedFcs);
+
+
+    ethFcs->markImmutable();
+    pkt->pushTrailer(ethFcs);
+    send(pkt, "ethOut");
     EV_TRACE << "packet parsed and sent out on gate ethOut" << endl;
 }
 
@@ -86,6 +103,9 @@ void SmolTcpStack::initialize(int stage)
     if (stage == inet::INITSTAGE_LAST) {
         auto ift = check_and_cast<inet::IInterfaceTable *>(getModuleByPath("^.interfaceTable"));
         auto intf = ift->getInterfaceByName("eth");
+
+        std::cout << getId() << intf->getMacAddress() << intf->getIPv4Address();
+
         stack = make_smoltcp_stack(getId(), intf->getMacAddress().str().c_str(), intf->getIPv4Address().str().c_str());
         EV_TRACE << "stack made for module " << getId() << stack << endl;
 
@@ -102,25 +122,33 @@ void SmolTcpStack::initialize(int stage)
 
 void SmolTcpStack::handleMessage(cMessage *msg)
 {
-    if (auto frame = dynamic_cast<inet::EthernetIIFrame*>(msg)) {
-        EV_TRACE << "received an ethernet frame" << endl;
+    if (auto packet = dynamic_cast<inet::Packet*>(msg)) {
+        EV_TRACE << "received a packet" << endl;
 
-        inet::serializer::EthernetSerializer ser;
-        inet::serializer::Context ctx;
         int N = 2048;
         unsigned char bytes[N];
-        inet::serializer::Buffer buf(bytes, N);
-        ser.serializePacket(frame, buf, ctx);
 
-        std::vector<unsigned char> b;
-        b.resize(N - buf.getRemainingSize());
-        for (size_t i = 0; i < N - buf.getRemainingSize(); ++i)
-            b[i] =  bytes[i];
+        //packet->popHeader(B(PREAMBLE_BYTES + SFD_BYTES));
+        auto chunk = packet->peekDataBytes();/* AllBytes();*/
+        auto b = chunk->getBytes();
+/*
+        // add FCS
+        const auto& fcsChunk = std::make_shared<inet::EthernetFcs>();
+        uint32_t fcs = inet::serializer::ethernetCRC(b.data(), b.size());
+        fcsChunk->setFcs(fcs);
+        fcsChunk->setFcsMode(inet::FCS_COMPUTED);
+        packet->insertTrailer(fcsChunk);*/
+
+        chunk = packet->peekDataBytes();
+        b = chunk->getBytes();
 
         rxBuffer.push_back(b);
 
-        EV_TRACE << "added a buffer of " << N - buf.getRemainingSize() << " bytes to rxbuffer" << endl;
+        //EV_TRACE << "added a buffer of " << N - buf.getRemainingSize() << " bytes to rxbuffer" << endl;
+
+        EV_TRACE << "start poll" << endl;
         poll_smoltcp_stack(stack, simTime().inUnit(SIMTIME_MS));
+        EV_TRACE << "end poll" << endl;
     } else {
         EV_WARN << "got message of unsupported class " << msg->getClassName() << endl;
     }
